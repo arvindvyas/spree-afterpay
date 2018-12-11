a9dc0d63aa070ef57bff7ac7883734e2969af137
Arvind Vyas <arvindvyas07@gmail.com>
2018-12-11 19:31:29 +0530

    change path

--- app/overrides/spree/checkout/_payment.rb
+++ app/overrides/spree/checkout/_payment.rb
@@ -2,5 +2,5 @@ Deface::Override.new(
   virtual_path: 'spree/checkout/_payment',
   name: 'Add afterpay script on payment page',
   insert_before: '[data-hook="payment_fieldset_wrapper"]',
-  text: '<%= render "spree/checkout/payment/afterpay_checkout"%>'
+  text: '<%= render "spree/checkout/payment/afterpay"%>'
 )
